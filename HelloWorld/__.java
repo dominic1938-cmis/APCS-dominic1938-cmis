082cd8973a7f93a228a9ec19953cebd3cacd9383
Dominic <dominic1938@cmis.ac.th>
2018-08-09 14:31:03 +0700

    Finish HelloWorld

---
+++ HelloWorld/__.java
@@ -0,0 +1,9 @@
+public class __
+    {
+    public static void main (String args[] )
+        {
+        System.out.println( "Hellow everybody SAM there!" );
+        
+        } //end method main
+    
+    } // end class HelloWorld
