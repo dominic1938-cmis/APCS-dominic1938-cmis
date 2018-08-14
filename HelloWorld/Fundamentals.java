3e3cefba42abcff8543ab4976339a5d881a34429
Dom <dominic1938@cmis.ac.th>
2018-08-14 09:35:42 +0700

    Started on datatype problems

---
+++ HelloWorld/Fundamentals.java
@@ -0,0 +1,15 @@
+public class Fundamentals
+    {
+    public static void main (String args[] ){
+        dataTypes();
+    }
+
+    public static void dataTypes(){
+        System.out.println("Byte");
+    }
+    
+    public static int add(int a, int b){
+        
+    }
+    
+    } 
