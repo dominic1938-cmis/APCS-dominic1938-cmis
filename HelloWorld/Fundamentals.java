be703fda27bd9797d424071974aa0e65c30f2439
Dom <dominic1938@cmis.ac.th>
2018-08-15 11:56:43 +0700

    3 examples of stuff

--- HelloWorld/Fundamentals.java
+++ HelloWorld/Fundamentals.java
@@ -5,7 +5,17 @@ public class Fundamentals
     }
 
     public static void dataTypes(){
-        System.out.println("Byte");
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float f = 234.5f;
+        double d = 123.4;
+        boolean b2 = true;
+        char c = 'A';
+        String Bite = "A Byte can store the number of toes, fingers and dicount value";
+        String Shrt = "A short can store the price of lego sets, price of lego brick, price of hair cuts"; 
+        String Int = "A int can store the price of lego minifigures, ";
     }
     
     public static int add(int a, int b){
