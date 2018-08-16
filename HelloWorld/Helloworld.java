ff198eee3eade7a79391bd0c62991a0f912728df
Dom <dominic1938@cmis.ac.th>
2018-08-16 14:36:53 +0700

    Binary numbers

--- HelloWorld/Helloworld.java
+++ HelloWorld/Helloworld.java
@@ -3,7 +3,8 @@ public class Helloworld
     public static void main (String args[] )
         {
         System.out.println( "Hellow everybody SAM there!" );
-        
+        byte n = 0b01111111;
+        System.out.println(n);
         } //end method main
     
-    } // end class HelloWorld
+    }
