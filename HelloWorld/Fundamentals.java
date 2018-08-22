734a5ca86b276766b3f6a42b1c9cfbe277865657
dom <dominic1938@cmis.ac.th>
2018-08-22 14:35:12 +0700

    Q1 finished

--- HelloWorld/Fundamentals.java
+++ HelloWorld/Fundamentals.java
@@ -17,7 +17,11 @@ public class Fundamentals
         String Shrt = "A short can store the price of lego sets, price of lego brick, price of hair cuts"; 
         String Int = "A int can store the price of lego minifigures, dog, laptop";
         String Lng = "A long can store the length of, our solar system in nanometers, biggest star we know in micrometers, mass of moon";
-        String Flt = "";
+        String Flt = "A float can store 1000000 lightyears in cm, the milkyway in nanometers, biggest blackhole in milimeters";
+        String dbl = "A double can store it can store 100000000 lightyears in cm, the known universe in cm,a quintillion Jeff Bezos's net worth";
+        String bool = "A bool represent life is suffering, all living things die eventually, Hearthstone is skill based game";
+        String chr = "A char can store the price of keyboards, price of chocolate, price of curtains";
+        System.out.format("Example data types: %n byte : %d%n short : %d%n int : %d%n long : %d%n float : %d%n double : %s boolean : %s char");
     }
     
     } 
