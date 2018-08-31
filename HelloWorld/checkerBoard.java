175b58bc57f362bf95fbec260461f43c5408d87b
dom <dominic1938@cmis.ac.th>
2018-08-31 12:00:33 +0700

    checker border

--- HelloWorld/checkerBoard.java
+++ HelloWorld/checkerBoard.java
@@ -9,23 +9,22 @@ public class checkerBoard {
 
     public static String checkerBoard (int w, int h){
         String output = "";
-        String mid = "";
+        String mid1 = "";
+        String mid2 = "";
         for (int a = 0; a <= h + 1; a++){
             if (a == 0){
                 for (int c = 0; c < w; c++){
-                    mid += "-";
+                    mid1 += "-";
                 }
-                output += "+" + mid + "+";
+                output += "+" + mid1 + "+";
             }
-            for (int b = 0; b <= w + 1; b++){
-                if (b == 0){
-                    for (int d = 0; d < w; d++){
-                        mid += "|\n";
-                    }
-                    output += "+" + mid + "+";
+            else if (a == h + 1){
+                for (int d = 0; d < w; d++){
+                    mid2 += "-";
                 }
+                output += "+" + mid2 + "+";
             }
-            if (a > 0){
+            else{ 
                 for (int b = 0; b < w; b++){
                     output += ".";
                 }
@@ -33,5 +32,5 @@ public class checkerBoard {
             output += "\n";
         }
         return output;
-    }
+   }  
 }
