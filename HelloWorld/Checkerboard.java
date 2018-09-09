fc6fb9c39686675c6cfeb84d037585c51f643bf0
dominic1938-cmis <42160990+dominic1938-cmis@users.noreply.github.com>
2018-09-09 20:30:43 +0700

    FINISHED CHECKERBOARD

--- HelloWorld/Checkerboard.java
+++ HelloWorld/Checkerboard.java
@@ -2,6 +2,9 @@ public class Checkerboard {
     public static void check(int h, int w){
         String mid = "";
         String mid2 = "";
+        int randomNum = (int)(Math.random() * ((10 - 1) + 1));
+        int randomY = (int)(Math.random() * ((h - 1) + 1)) + 1;
+        int randomX = (int)(Math.random() * ((w - 1) + 1)) + 1;
         for (int x = 1; x <= w; x++)
         {
             mid += ("-");
@@ -10,9 +13,16 @@ public class Checkerboard {
         for(int y = 0; y < h; y++){
             System.out.print("|");
             for (int x = 0; x < w; x++){
-                if (y % 2 != 0){ // if line is odd
-                    if (x % 2 != 0){ // if spot is odd
-                        System.out.print("#");
+                if (y % 2 != 0){
+                    if (x % 2 != 0){
+                        if ((randomY == y || randomY == y + 1) && (randomX == x || randomX == x+1)){
+                            System.out.print(randomNum);
+                            randomX = 0;
+                            randomY = 0;
+                        }
+                        else {
+                            System.out.print("#");
+                        }
                     }
                     else{
                         System.out.print(" ");
@@ -21,9 +31,16 @@ public class Checkerboard {
                         System.out.print("| \n");
                     }
                 }
-                if (y % 2 == 0){ // if line is even
-                    if (x % 2 == 0){ // if spot is odd
-                        System.out.print("#");
+                if (y % 2 == 0){
+                    if (x % 2 == 0){
+                        if ((randomY == y || randomY == y + 1) && (randomX == x || randomX == x+1)){
+                            System.out.print(randomNum);
+                            randomX = 0;
+                            randomY = 0;
+                        }
+                        else {
+                            System.out.print("#");
+                        }
                     }
                     else{
                         System.out.print(" ");
--- HelloWorld/Fundamentals.java
+++ HelloWorld/Fundamentals.java
@@ -131,4 +131,3 @@ public class Fundamentals
         return output;
     }
 }
-
