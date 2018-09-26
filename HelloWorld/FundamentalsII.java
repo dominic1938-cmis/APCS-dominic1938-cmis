fcfecdd886b8d53ee931e4abf4716c47377b8075
dom <dominic1938@cmis.ac.th>
2018-09-26 13:26:54 +0700

    parus

--- HelloWorld/FundamentalsII.java
+++ HelloWorld/FundamentalsII.java
@@ -25,6 +25,57 @@ public class FundamentalsII{
         }
     }
 
+    public static void printArray(double[] arr, boolean skip){
+        int x = 0;
+        int y = 0;
+        if (skip == true){
+            while (x < arr.length){
+                System.out.println(arr[x]);
+                x += 2;
+            }
+        }
+        else if (skip == false){
+            while (y < arr.length){
+                System.out.println(arr[y]);
+                y += 1;
+            }
+        }
+    }
+
+    public static void printArray(String[] arr, boolean skip){
+        int x = 0;
+        int y = 0;
+        if (skip == true){
+            while (x < arr.length){
+                System.out.println(arr[x]);
+                x += 2;
+            }
+        }
+        else if (skip == false){
+            while (y < arr.length){
+                System.out.println(arr[y]);
+                y += 1;
+            }
+        }
+    }
+
+    public static void printArray(boolean[] arr, boolean skip){
+        int x = 0;
+        int y = 0;
+        if (skip == true){
+            while (x < arr.length){
+                System.out.println(arr[x]);
+                x += 2;
+            }
+        }
+        else if (skip == false){
+            while (y < arr.length){
+                System.out.println(arr[y]);
+                y += 1;
+            }
+        }
+    }
+
     public static int lastItem(int[] array){
         int x = 0;
         x = (array.length - 1);
@@ -45,27 +96,51 @@ public class FundamentalsII{
         int x = (array.length - 1);
         return array[x];
     }
-    
+
     public static int middleItem(int[] array){
-        int x = (array.length / 2);
-        return array[x];
+        if (array.length % 2 != 0){
+            int x = array[array.length / 2]                                                                                                                                                                                                                                                                                                                                                                                                                                                      ;
+            return x;
+        }
+        else{
+            int x = array[(array.length / 2) - 1];
+            return x;
+        }
     }
-    
+
     public static double middleItem(double[] array){
-        int x = (array.length / 2);
-        return array[x];
+        if (array.length % 2 != 0){
+            double x = array[array.length / 2]                                                                                                                                                                                                                                                                                                                                                                                                                                                      ;
+            return x;
+        }
+        else{
+            double x = array[(array.length / 2) - 1];
+            return x;
+        }
     }
-    
+
     public static String middleItem(String[] array){
-        int x = (array.length / 2);
-        return array[x];
+        if (array.length % 2 != 0){
+            String x = array[array.length / 2]                                                                                                                                                                                                                                                                                                                                                                                                                                                      ;
+            return x;
+        }
+        else{
+            String x = array[(array.length / 2) - 1];
+            return x;
+        }
     }
-    
+
     public static boolean middleItem(boolean[] array){
-        int x = (array.length / 2);
-        return array[x];
+        if (array.length % 2 != 0){
+            boolean x = array[array.length / 2]                                                                                                                                                                                                                                                                                                                                                                                                                                                      ;
+            return x;
+        }
+        else{
+            boolean x = array[(array.length / 2) - 1];
+            return x;
+        }
     }
-    
+
     public static int[] randomInts(int n, int min, int max){
         int[] array = new int[n];
         for (int x = 0; x < n; x++){
@@ -74,7 +149,7 @@ public class FundamentalsII{
         }
         return array;
     }
-    
+
     public static double[] randomDouble(int n, double min, double max){
         double [] array = new double[n];
         for (int x = 0; x < n; x++){
@@ -83,6 +158,28 @@ public class FundamentalsII{
         }
         return array;
     }
+
+    public static int[] copy(int[] array){
+        int [] cop = array;
+        return cop;
+    }
+
+    public static double[] copy(double[] array){
+        double [] cop = array;
+        return cop;
+    }
+
+    public static String[] copy(String[] array){
+        String [] cop = array;
+        return cop;
+    }
+
+    public static Boolean[] copy(Boolean[] array){
+        Boolean [] cop = array;
+        return cop;
+    }
     
-    
+    public static int[] pairs(int n){
+        
+    }
 }
