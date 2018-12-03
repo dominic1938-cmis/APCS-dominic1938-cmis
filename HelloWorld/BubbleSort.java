53fe3abf7a3398f4368fd03de58520f50f799972
as <as>
2018-12-03 14:35:55 +0700

    a

---
+++ HelloWorld/BubbleSort.java
@@ -0,0 +1,46 @@
+public class BubbleSort
+{
+    public static void main(String[] args){
+        int[] array = getArray(20, true);
+        print(array);
+        sort(array);
+        print(array);
+    }
+    
+    public static int[] sort(int[] array){
+        boolean swap = true;
+        int y = 0;
+        for (int x = 0; x < array.length - 1; x++)
+        {
+            if (array[x] > array[y+1])
+            {
+                array[x] = array[y+1];
+            }
+            y++;
+        }
+        return array; 
+    }
+
+    public static int[] getArray(int w, boolean random){
+        int[] array = new int[w];
+        int ct = 0;
+        for(int i = 0; i < w; i++){
+            if(random)
+                array[i] = (int)(Math.random() * 20000) * (Math.random() > 0.4 ? -1 : 1);
+            else
+                array[i] = ct++;
+        }
+        return array;
+    }
+
+    public static void print(int[] array){
+        String out = "{";
+        for(int i = 0; i < array.length; i++){
+            out += array[i];
+            if( i != array.length -1)
+                out+=", ";
+        }
+        out+= "}\n";
+        System.out.println(out);
+    }
+}
