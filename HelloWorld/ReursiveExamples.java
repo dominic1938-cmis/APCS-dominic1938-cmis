b536f68c6034d0abac81d876e1cf68fa28127ece
gosh <a>
2019-01-17 14:35:02 +0700

    yea

--- HelloWorld/ReursiveExamples.java
+++ HelloWorld/ReursiveExamples.java
@@ -1,40 +1,79 @@
+
 public class ReursiveExamples
 {
-    
     public static int multiply(int a, int b)
     {
-        if (b == 1)
+        if (a < 0 && b < 0)
         {
-            return a;
+            if (b == 1)
+            {
+                return a;
+            }
+
+            else if (b == 0 || a ==0)
+            {
+                return 0;
+            }
+
+            else {
+                return a + multiply(a, (b - 1));
+            }
         }
-        
-        else if (b == 0 || a ==0)
+        else
         {
-            return 0;
-        }
-        
-        else {
-            return a + multiply(a, (b - 1));
+            if (b == 1)
+            {
+                return a;
+            }
+
+            else if (b == 0 || a ==0)
+            {
+                return 0;
+            }
+
+            else {
+                return a + multiply(a, (b - 1));
+            }
         }
     }
-    
+
     public static int divide(int a, int b)
     {
         if (b == 1)
         {
             return a;
         }  
-        
-        else if (a == 0)
+
+        else if (a <= b)
         {
             return 0;
         }
-        
+
+        else if (a == b)
+        {
+            return 1;
+        }
+
         else {
-            return 1 + divide((a - b), b);
+            return 1 + divide(a - b, b);
+        }
+    }
+
+    public static int power(int a, int b)
+    {
+        if (b == 0)
+        {
+            return 1;
+        }
+
+        else if (b == 1)
+        {
+            return a;
+        }
+
+        else
+        {
+            return multiply(a, power(a, b - 1));
         }
     }
 }
-
-
-
