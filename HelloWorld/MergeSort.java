b96bd94ac33e49d7677f827493b764f0a18fb210
a <a>
2019-01-22 12:04:32 +0700

    a

--- HelloWorld/MergeSort.java
+++ HelloWorld/MergeSort.java
@@ -20,13 +20,40 @@ public class MergeSort
     public static int[] merge(int[] array){
         int[] front = new int [array.length/2];
         int[] back = new int [array.length/2];
+        int temp = 0;
+        int f = 0;
+        int b = 0;
+        int count = 0;
         if (array.length == 1)
         {
-            
+            return array;
         }
         else
         {
-            return merge(front);
+            for (int i = 0; i < front.length; i++)
+            {
+                front[i] = array[i];
+            }
+            for (int i = array.length/2; i < array.length; i++)
+            {
+                back[i] = array[i];
+            }
+            front = merge(front);
+            back = merge(back);
+            while (f < front.length || b < back.length){
+                if (front[f] > back[b])
+                {
+                    array[count] = front[f];
+                    f++;
+                }
+                else
+                {
+                    array[count] = back[b];
+                    b++;
+                }
+                count++;
+            }
         }
+        return array;
     }
 }
