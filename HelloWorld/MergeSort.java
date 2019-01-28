6271a1361c5123e9acb86a6d6f0cca945da7995a
dominic1938-cmis <42160990+dominic1938-cmis@users.noreply.github.com>
2019-01-28 20:18:58 +0700

    i finally did it

--- HelloWorld/MergeSort.java
+++ HelloWorld/MergeSort.java
@@ -38,6 +38,39 @@ public class MergeSort
             }
             front = merge(front);
             back = merge(back);
+                    int f = 0;
+        int b = 0;
+        for (int count = 0; count < array.length; count ++)
+        {
+            if (front[f] <= back[b] && f != front.length)
+            {
+                array[count] = front[f];
+                f++;
+            }
+            else if (back[b] < front[f] && b != back.length)
+            {
+                array[count] = back[b];
+                b++;
+            }
+            if (f  == front.length)
+            {
+                count++;
+                for (int i = b; i < back.length; i++)
+                {
+                    array[count] = back[i];
+                    count++;
+                }
+            }
+            else if (b == back.length)
+            {
+                count++;
+                for (int i = f; i < front.length; i++)
+                {
+                    array[count] = front[i];
+                    count++;
+                }
+            }
+        }
             return array;
         }
     }
