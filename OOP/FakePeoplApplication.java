94c9324927e30052c354f5c81419a393aebecb00
a <a>
2019-02-22 11:54:13 +0700

    a

---
+++ OOP/FakePeoplApplication.java
@@ -0,0 +1,7 @@
+public class FakePeoplApplication
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
--- OOP/FakePeople.java
+++ OOP/FakePeople.java
@@ -1,4 +1,12 @@
 public class FakePeople
 {
+    private String name;
+    private String type;
+    private int age;
     
+    public String toString()
+    {
+        String out = "Name: " + this.name + "\nType: " + this.type + "\nAge: " + this.age + "\n";
+        return out;
+    }
 }
