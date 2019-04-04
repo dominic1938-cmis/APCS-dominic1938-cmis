cbfc5809ac7e42b7d51f85e4e3d1e6168f97eab7
dominic1938-cmis <42160990+dominic1938-cmis@users.noreply.github.com>
2019-04-04 13:13:43 +0700

    a

---
+++ Inheritance/Bird.java
@@ -0,0 +1,41 @@
+
+public class Bird extends Animal
+{
+    private String color;
+    
+    public Bird(int x, int y, String color)
+    {
+        super(x, y);
+        this.color = color;
+    }
+
+    public void fly(int dx, int dy){
+        if(Math.abs(dx) > 1 && Math.abs(dy) > 1){
+            int newX = getLocation().get(0) + dx;
+            int newY = getLocation().get(1) + dy;
+            super. setLocation(newX, newY);
+        }
+    }
+    
+    public void setColor(String color)
+    {
+        this. color = color;
+    }
+    
+    public String getColor()
+    {
+        return color;
+    }
+
+    public boolean eat(String what)
+    {
+        if (what == "worms" || what == "seeds")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
