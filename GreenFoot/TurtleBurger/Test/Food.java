fde2046761d3423a11ebd6487b9ac888febc53d1
a <a>
2019-05-15 09:20:38 +0700

    a

--- GreenFoot/TurtleBurger/Test/Food.java
+++ GreenFoot/TurtleBurger/Test/Food.java
@@ -8,24 +8,7 @@ public class Food extends Actor
         int y = getY();
         int bx = ((MyWorld)getWorld()).getBerd().getX();
         int by = ((MyWorld)getWorld()).getBerd().getY();
-        int random = (int)Math.random();
-        if (bx > x)
-        {
-            x++;
-        }
-        else if (bx < x)
-        {
-            x--;
-        }
-
-        if (by > y)
-        {
-            y++;
-        }
-        else if (by < y)
-        {
-            y--;
-        }
-        setLocation(x,y);
+        turnTowards(bx, by);
+        move(2);
     }    
 }
