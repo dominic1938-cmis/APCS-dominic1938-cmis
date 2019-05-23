49b219c05e3602a0549db8e726272cc54ce3af4f
a <a>
2019-05-23 11:55:10 +0700

    a

--- GreenFoot/Test2/Battle.java
+++ GreenFoot/Test2/Battle.java
@@ -4,5 +4,22 @@ public class Battle extends World
     public Battle()
     {    
         super(600, 400, 1); 
+        prepare();
+    }
+    
+    public void prepare()
+    {
+        Meg meg = new Meg();
+        addObject(meg,562,290);
+        Barrier barrier = new Barrier();
+        addObject(barrier,588,169); barrier = new Barrier();
+        addObject(barrier,445,172); barrier = new Barrier();
+        addObject(barrier,525,171); barrier = new Barrier();
+        addObject(barrier,367,170); barrier = new Barrier();
+        addObject(barrier,290,174); barrier = new Barrier();
+        addObject(barrier,232,151); barrier = new Barrier();
+        addObject(barrier,162,154); barrier = new Barrier();
+        addObject(barrier,46,135); barrier = new Barrier();
+        addObject(barrier,105,127); barrier = new Barrier();
     }
 }
